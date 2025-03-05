e1ecbb3b5e94b7ab6045e65a76f9437e60761527
MasterOfBlindfish <45813612+MasterOfBlindfish@users.noreply.github.com>
2025-03-05 14:47:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,8 @@ apt-get install curl xrdp xfce4 xfce4-goodies -y
 #sudo curl -fsSL https://tailscale.com/install.sh | sh
 #sudo apt-get install language-pack-kde-de -y
 
+adduser dietpi
+usermod -aG sudo dietpi
+
 # Grant sudo without password
 echo 'dietpi ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
